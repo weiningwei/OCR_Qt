74445784a118a3eb3baa53ab10ad8f3c0ea287b6
weiningwei <1479796521@qq.com>
2024-11-17 22:12:43 +0800

    ✨ feat(ui): ui初步搭建，可读取文件

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,10 +2,28 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->label->setText("");
+
+    connect(this, SIGNAL(sig_show_img()), this, SLOT(show_img()));
+}
+
+void MainWindow::show_img()
+{
+    // qDebug() << "show_img";
+    if (_img.isNull())
+    {
+        QMessageBox::warning(this, "warning", "Plense input image!");
+        return;
+    }
+//    ui->label->setPixmap(_img);
+    QPixmap dest = _img.scaled(ui->label->size(), Qt::KeepAspectRatio);
+    ui->label->setPixmap(dest);
+
+    ui->label->show();
+    return;
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +31,37 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_pushButton_file_clicked()
+{
+    QString file_path = QFileDialog::getOpenFileName(
+        this,
+        tr("select a file."),
+        ".",// D:/
+        tr("images(*.png *jpeg *bmp *jpg);;All files(*.*)"));
+    if (file_path.isEmpty())
+    {
+        QMessageBox::warning(this, "Warning!", "Failed to select the image!");
+        return;
+    }
+    _img_path = file_path;
+    bool status = _img.load(_img_path);
+    if (!status)
+    {
+        QMessageBox::warning(this, "Error!", "Failed to load the image!");
+        return;
+    }
+    emit sig_show_img();
+}
+
+void MainWindow::on_pushButton_copy_clicked()
+{
+}
+
+void MainWindow::on_pushButton_screen_clicked()
+{
+}
+
+void MainWindow::on_pushButton_ocr_clicked()
+{
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QPixmap>
+#include <QFile>
+#include <QFileDialog>
+#include <QDir>
+#include <QDebug>
+#include <QMessageBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +22,27 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+// 自定义信号
+signals:
+    // 信号没有返回值，可以有参数,信号函数不需要定义，只需要声明
+    void sig_show_img();
+
+// 自定义槽函数
+public slots:
+    void show_img();
+
+private slots:
+    void on_pushButton_file_clicked();
+
+    void on_pushButton_copy_clicked();
+
+    void on_pushButton_screen_clicked();
+
+    void on_pushButton_ocr_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QString _img_path;
+    QPixmap _img;
 };
 #endif // MAINWINDOW_H
